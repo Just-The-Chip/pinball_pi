e94d26f60b01bf21cddb8050328b79dc4f2631d3
Elgin Lakebrink <sasocki20@yahoo.com>
2026-07-01 23:34:55 -0500

    new player test code for testing player with all current sounds.

--- sound/player_test.py
+++ sound/player_test.py
@@ -11,5 +11,9 @@ from player import Player
 
 #p = Path('C:\\Users\\sasoc\\Desktop\\Nerd Night\\Pinball sounds\\sorted_sounds\\component sounds\\slider sound.wav')
 player = Player()
-player.play("points_button")
-time.sleep(3)
+
+all_sounds = ["points_button","pop_bumper","spinner_points","mario_tube","bikes","game_start","game_end","round_end","circus","slingshot","rear_popper","finale_reject","ball_save_solenoid","finale_unlock","multiball_deposit","unlock_left_launcher","double_kill","headshot","triple_kill","goat","ball_drain_save","slide1","slide2","slide3","slide4","slide5","slide7"]
+
+for sound in all_sounds:
+    player.play(sound)
+    time.sleep(8)
